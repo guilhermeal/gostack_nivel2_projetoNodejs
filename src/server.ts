6a5bcb0d917a23b4872732dc7eecc985f5b4a49b
Guilherme Almeida <guiallan.al@gmail.com>
2020-09-13 17:15:36 -0300

    feat(cors): add package cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+
 import 'express-async-errors';
 
 import routes from './routes';
@@ -13,6 +15,7 @@ const port = 3333;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
